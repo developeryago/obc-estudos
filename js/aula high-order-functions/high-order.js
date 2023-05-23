2d6757d6733686aa24a9c8caa22f63dea95b579a
developeryago <contato.yagomachado@outlook.com>
2023-05-23 09:39:13 -0300

    estudos sobre high order functions

--- js/aula high-order-functions/high-order.js
+++ js/aula high-order-functions/high-order.js
@@ -1,5 +1,44 @@
 const personagens = [
-    {nome: "Cadu", animal: "pinguim", esporte: "surfe"},
-    {nome: "João", animal: "frango", esporte: "surfe"},
-    {nome: "Big Z", animal: "pinguim", esporte: "surfe"}
+    {nome: "Cadu", animal: "pinguim", esporte: "surfe", nota: 27},
+    {nome: "João", animal: "frango", esporte: "surfe", nota: 26},
+    {nome: "Big Z", animal: "pinguim", esporte: "surfe", nota: 30}
 ]
+
+// map
+/*const nomes = []
+
+for (let i = 0; i < personagens.length; i++){
+    nomes.push(personagens[i].nome)
+}*/
+const nomes = personagens.map (function(personagem){
+    return  personagem.nome
+})
+console.log(nomes)
+
+// filter
+//const pinguins = []
+
+/*for (i = 0; i < personagens.length; i++){
+    if (personagens[i].animal == "pinguim"){
+        pinguins.push(personagens[i])
+    }
+//}*/
+
+const pinguins = personagens.filter(function(personagem){
+    return personagem.animal === "pinguim"
+})
+console.log(pinguins)
+
+// reduce
+const somaNotas = personagens.reduce(function(value, personagem){
+    return value + personagem.nota
+}, 0)
+
+console.log(somaNotas)
+
+// sort
+const novoPersonagens = personagens.slice().sort(function(a, b){
+    return a.nota - b.nota
+})
+console.log(personagens)
+console.log(novoPersonagens)
