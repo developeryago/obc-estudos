f375948881dd9dbaa9307ae159f1aecb0ec3b19e
developeryago <contato.yagomachado@outlook.com>
2023-07-03 11:49:16 -0300

    Update index.js

--- js/modulo-de-exercícios/index.js
+++ js/modulo-de-exercícios/index.js
@@ -1,3 +1,7 @@
 const med = (...numbers) => numbers.reduce((a, b) => a + b, 0) / numbers.length
 
-console.log(med(4, 4, 10))
+
+
+console.log(med(2, 5, 7, 1, -2))
+console.log(med(10, 10, 10, 10, 9))
+console.log(med(25, 75))
