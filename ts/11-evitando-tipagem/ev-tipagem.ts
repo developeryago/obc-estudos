e9f679c39060cb6bb1c3fa28cc3bf3822d64f4e6
developeryago <contato.yagomachado@outlook.com>
2023-07-26 09:53:31 -0300

    Aula sobre como evitar a tipagem no TS

---
+++ ts/11-evitando-tipagem/ev-tipagem.ts
@@ -0,0 +1,16 @@
+function victory (club:{name: string, position?: number})  {
+    // acrescenta três pontos ao clube 
+}
+
+victory({name: "Liverpool", position: 1})
+victory({name: "Fluzão"})
+
+function verification(test) {
+    if (test) {
+
+    } else {
+        let _check: never
+        let text = _check
+        text = ""
+    }
+}
