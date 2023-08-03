4de13f625787345f571d54fef3d3e8f0125c9e64
developeryago <contato.yagomachado@outlook.com>
2023-08-03 18:37:31 -0300

    finalização Exercício sobre interfaces

--- ts/ex03/dist/index.js
+++ ts/ex03/dist/index.js
@@ -8,66 +8,99 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let allUsers = [];
+const allUsers = [];
 function getUser(name) {
     return __awaiter(this, void 0, void 0, function* () {
-        const responde = yield fetch(`https://api.github.com/users/${name}`);
-        const userJson = yield responde.json();
-        saveUser(userJson);
+        const response = yield fetch(`https://api.github.com/users/${name}`);
+        const userJson = yield response.json();
+        if (userJson.message) {
+            console.log("Usuário não encotrado");
+        }
+        else {
+            allUsers.push(userJson);
+            console.log(`O Usuário ${userJson.name} foi salvo!
+            id: ${userJson.id}
+            login: ${userJson.login}
+            bio: ${userJson.bio}
+            repositórios: ${userJson.public_repos}
+            url: ${userJson.repos_url}`);
+        }
+        // saveUser(userJson)
     });
 }
-function saveUser(userJson) {
-    const user = {
-        name: userJson.name,
-        id: userJson.id,
-        login: userJson.login,
-        bio: userJson.bio,
-        public_repos: userJson.public_repos,
-        repos_url: userJson.repos_url
-    };
-    allUsers.push(user);
-    return user;
-}
-function showUserRepository(name) {
+// function saveUser(userJson: User) {
+//     const user: User = {
+//         name: userJson.name,
+//         id: userJson.id,
+//         login: userJson.login,
+//         bio: userJson.bio,
+//         public_repos: userJson.public_repos,
+//         repos_url: userJson.repos_url
+//     }
+//     return user
+// }
+function showUserRepository(userName) {
     return __awaiter(this, void 0, void 0, function* () {
-        const responde = yield fetch(`https://api.github.com/users/${name}/repos`);
-        const userJson = yield responde.json();
-        UserRepository(userJson);
+        const user = allUsers.find(user => user.login === userName);
+        if (typeof user !== "undefined") {
+            const response = yield fetch(user.repos_url);
+            const repos = yield response.json();
+            let message = "";
+            repos.forEach(repo => {
+                message += `Repositórios: 
+                Nome: ${repo.name}
+                Descrição: ${repo.description}
+                Estrelas: ${repo.stargazers_count}
+                É u fork: ${repo.fork ? "Sim" : "Não"} \n`;
+            });
+            console.log(message);
+            // UserRepository(userJson)
+        }
+        else {
+            console.log("Usuário inválido!");
+        }
     });
 }
-function UserRepository(user) {
-    const repos = {
-        name: user.name,
-        description: user.description,
-        fork: user.fork,
-        stargazers_count: user.stargazers_count
-    };
-    console.log(repos);
-}
+// function UserRepository(user: UserRepository) {
+//     const repos: UserRepository = {
+//         name: user.name,
+//         description: user.description,
+//         fork: user.fork,
+//         stargazers_count: user.stargazers_count
+//     }
+//     console.log(repos)
+// }
 function sumRepository() {
     let sum = 0;
     allUsers.forEach(user => {
         sum += user.public_repos;
     });
-    return sum;
+    console.log(`Soma total do número de repositórios: ${sum}`);
 }
 function showAllUsers() {
     let list = `Lista de Usuários pesquisados: \n`;
     allUsers.forEach(user => {
-        list += ` - ${user.name}`;
+        list += ` - ${user.login}`;
     });
-    return list;
+    console.log(list);
 }
 function showTop5Users() {
-    const userArray = [];
-    let valueRepos;
-    allUsers.forEach(user => {
-        if (user.public_repos > valueRepos) {
-            valueRepos = user.public_repos;
-            userArray.unshift(user);
-        }
+    const userArray = allUsers.slice().sort((user1, user2) => user2.public_repos - user1.public_repos).slice(0, 5);
+    let list = `Usuários com mais repositórios: \n`;
+    userArray.forEach(user => {
+        list += `${user.login} com ${user.public_repos} repositórios \n`;
     });
-    return userArray;
+    console.log(list);
 }
-getUser("developeryago");
-showUserRepository("developeryago");
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield getUser("developeryago");
+        yield getUser("gustavoguanabara");
+        yield getUser("isaacpontes");
+        yield showUserRepository("gustavoguanabara");
+        yield showAllUsers();
+        yield sumRepository();
+        showTop5Users();
+    });
+}
+main();
--- ts/ex03/src/index.ts
+++ ts/ex03/src/index.ts
@@ -1,4 +1,4 @@
-let allUsers: User[] = []
+const allUsers: User[] = []
 
 interface User {
     name: string,
@@ -6,7 +6,8 @@ interface User {
     login: string,
     bio: string,
     public_repos: number,
-    repos_url:string
+    repos_url:string,
+    message?: "Not Found!"
 }
 
 interface UserRepository {
@@ -17,68 +18,104 @@ interface UserRepository {
 }
 
 async function getUser(name: string) {
-    const responde = await fetch(`https://api.github.com/users/${name}`)
-    const userJson = await responde.json()
-    saveUser(userJson)
-}
-
-function saveUser(userJson: User) {
-    const user: User = {
-        name: userJson.name,
-        id: userJson.id,
-        login: userJson.login,
-        bio: userJson.bio,
-        public_repos: userJson.public_repos,
-        repos_url: userJson.repos_url
+    const response = await fetch(`https://api.github.com/users/${name}`)
+    const userJson: User = await response.json()
+    if (userJson.message) {
+        console.log("Usuário não encotrado")
+    } else {
+        allUsers.push(userJson)
+        console.log(
+            `O Usuário ${userJson.name} foi salvo!
+            id: ${userJson.id}
+            login: ${userJson.login}
+            bio: ${userJson.bio }
+            repositórios: ${userJson.public_repos}
+            url: ${userJson.repos_url}`
+        )
     }
-    allUsers.push(user)
-    return user
+    // saveUser(userJson)
 }
 
-async function showUserRepository (name: string) {
-    const responde = await fetch(`https://api.github.com/users/${name}/repos`)
-    const userJson = await responde.json()
-    UserRepository(userJson)
-}
+// function saveUser(userJson: User) {
+//     const user: User = {
+//         name: userJson.name,
+//         id: userJson.id,
+//         login: userJson.login,
+//         bio: userJson.bio,
+//         public_repos: userJson.public_repos,
+//         repos_url: userJson.repos_url
+//     }
+    
+//     return user
+// }
 
-function UserRepository(user: UserRepository) {
-    const repos: UserRepository = {
-        name: user.name,
-        description: user.description,
-        fork: user.fork,
-        stargazers_count: user.stargazers_count
+async function showUserRepository (userName: string) {
+    const user = allUsers.find(user => user.login === userName)
+    if (typeof user !== "undefined") {
+        const response = await fetch(user.repos_url)
+        const repos: UserRepository[] = await response.json()
+        let message = ""
+        repos.forEach( repo => {
+            message += `Repositórios: 
+                Nome: ${repo.name}
+                Descrição: ${repo.description}
+                Estrelas: ${repo.stargazers_count}
+                É u fork: ${repo.fork? "Sim" : "Não"} \n`
+        })
+        console.log(message)
+        // UserRepository(userJson)
+    } else {
+        console.log("Usuário inválido!")
     }
-    console.log(repos)
+    
 }
+
+// function UserRepository(user: UserRepository) {
+//     const repos: UserRepository = {
+//         name: user.name,
+//         description: user.description,
+//         fork: user.fork,
+//         stargazers_count: user.stargazers_count
+//     }
+//     console.log(repos)
+// }
 
 function sumRepository() {
     let sum = 0
     allUsers.forEach(user => {
        sum +=  user.public_repos
     })
-    return sum
+    console.log(`Soma total do número de repositórios: ${sum}`)
 }
 
 function showAllUsers () {
     let list = `Lista de Usuários pesquisados: \n`
 
     allUsers.forEach(user => {
-        list += ` - ${user.name}`
+        list += ` - ${user.login}`
     })
-    return list
+    console.log(list)
 }
 
 function showTop5Users () {
-    const userArray: User[] = []
-    let valueRepos: number
-    allUsers.forEach( user => {
-        if (user.public_repos > valueRepos) {
-            valueRepos = user.public_repos
-            userArray.unshift(user)
-        }
+    const userArray = allUsers.slice().sort((user1, user2) => user2.public_repos - user1.public_repos).slice(0, 5)
+    
+    let list = `Usuários com mais repositórios: \n`
+
+    userArray.forEach(user => {
+        list += `${user.login} com ${user.public_repos} repositórios \n`
     })
-    return userArray
+    console.log(list)
 }
 
-getUser("developeryago")
-showUserRepository("developeryago")
+async function main() {
+    await getUser("developeryago")
+    await getUser("gustavoguanabara")
+    await getUser("isaacpontes")
+    await showUserRepository("gustavoguanabara")
+    await showAllUsers()
+    await sumRepository()
+    showTop5Users()
+}
+
+main()
