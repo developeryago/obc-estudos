c79156b04292eddfa0227bb652b00102f41cd0b0
developeryago <contato.yagomachado@outlook.com>
2023-08-04 19:18:17 -0300

    Aula sobre POO com TS

---
+++ ts/15-poo-no-ts/poo.ts
@@ -0,0 +1,36 @@
+class Spaceship {
+    private name: string
+    protected captain: string
+    protected speed: number
+    constructor(name, captain) {
+      this.name = name
+      this.captain = captain
+      this.speed = 0
+    }
+  
+    accelerate(rate: number, time: number) {
+      this.speed = rate * time
+    }
+  }
+  
+  class Fighter extends Spaceship {
+    weapons: number
+    constructor(name: string, captain: string, weapons: number) {
+      super(name, captain)
+      this.weapons = weapons
+    }
+  
+    shoot() {
+      for (let i = 0; i < this.weapons; i++) {
+        console.log('Pew!')
+      }
+    }
+  
+    erase() {
+      this.captain = ''
+    }
+  }
+  
+  let ship = new Spaceship('USS Enterprise', 'James T. Kirk')
+  
+  ship.accelerate(50, 10)
