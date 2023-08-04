949b8e25f64c721c2d594027d0ec18056b9c03c9
developeryago <contato.yagomachado@outlook.com>
2023-08-04 18:29:15 -0300

    Aulas sobre módulos no TS

---
+++ ts/14-modulos/import.ts
@@ -0,0 +1,10 @@
+import { Team } from "./export";
+
+const Liverpool: Team = {
+    name: "Liverpool",
+    nationality: "England",
+    titles: 50,
+    fans: 9999999999999
+}
+
+console.log(Liverpool)
