90fdfa1decae5395703bcbcdeecb02f88266bf2f
developeryago <contato.yagomachado@outlook.com>
2023-08-07 19:53:41 -0300

    Aula sobre decorators

---
+++ ts/17-decorators/decorators.ts
@@ -0,0 +1,26 @@
+function Decorator() {
+    return function (target, key, descriptor) {
+        console.log('Chamando Decorator')
+      }
+  }
+  
+  class Planet {
+    name: string
+  
+    constructor(name: string) {
+      this.name = name
+    }
+  
+    @Decorator()
+    calculate(value: number) {
+      // ...
+      console.log(`Calculando ${value} * 2`);
+      return value * 2
+    }
+  }
+  
+  const planet = new Planet('Terra')
+  
+  const result = planet.calculate(5)
+  
+  console.log(`Resultado: ${result}`)
