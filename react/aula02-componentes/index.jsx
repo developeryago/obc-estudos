aa293b8e2d679fc6270856a101365b3833d70078
Yago Machado <contato.yagomachado@outlook.com>
2023-12-05 08:23:38 -0300

    Aula sobre componentes React

---
+++ react/aula02-componentes/index.jsx
@@ -0,0 +1,12 @@
+function Button() {
+    return <button>Meu Botão</button>
+}
+
+function Container() {
+    return (
+        <div className="container">
+            <h1>H1 dentro do container</h1>
+            <p>Parágrafo dentro do container</p>
+        </div>
+    )
+}
