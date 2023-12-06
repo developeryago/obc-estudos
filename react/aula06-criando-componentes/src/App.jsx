8ba583e50ac4e73ce5cd16e1fd59a6c4053e0017
Yago Machado <contato.yagomachado@outlook.com>
2023-12-06 15:56:24 -0300

    Manipulando componentes + Sass com React

--- react/aula06-criando-componentes/src/App.jsx
+++ react/aula06-criando-componentes/src/App.jsx
@@ -1,16 +1,16 @@
-import Title from './components/Title'
-import './App.css'
+import Title from "./components/Title"
 import SubTitle from './components/SubTitle'
 import H1  from './components/H1'
+import styles from "./styles/css/App.module.css"
 
 
 function App() {
   return ( 
-    <>
+    <div className={styles.app}>
     <Title></Title>
     <SubTitle></SubTitle>
     <H1></H1>
-    </>
+    </div>
   )
 }
 export default App 
--- react/aula06-criando-componentes/src/components/H1.jsx
+++ react/aula06-criando-componentes/src/components/H1.jsx
@@ -1,4 +1,6 @@
-export function H1() {
-    const frame = "React"
-    return <h1>{frame}</h1>
+export default function H1() {
+    const frame = false
+    return <h1 style={{
+      color: frame ? "#00ff9f" : "#4169e1"
+    }}>{frame ? "Meu futuro": "React"}</h1>
   }
