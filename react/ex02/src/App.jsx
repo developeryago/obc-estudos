bc3494b078d744c2309222d07345224b31206afb
Yago Machado <contato.yagomachado@outlook.com>
2023-12-07 17:29:51 -0300

    Modificação de componentes reutilizáveis

--- react/ex02/src/App.jsx
+++ react/ex02/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import Card from "./components/Card"
+import StwImg from "../public/img01.jpg"
+import GofImg from "../public/god-of-war.jpg"
+import Gta6 from "../public/gta6.jpg"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +11,15 @@ function App() {
   return (
     <>
       <div>
-          <Card></Card>
+          <Card 
+          title="Poster: Star Wars (1977)"
+          poster={StwImg}></Card>
+          <Card 
+          title="Poster: God of War (2018)"
+          poster={GofImg}></Card>
+          <Card 
+          title="Poster: GTA VI"
+          poster={Gta6}></Card>
       </div>
     </>
   )
--- react/ex02/src/components/Card.jsx
+++ react/ex02/src/components/Card.jsx
@@ -1,14 +1,12 @@
 import StarWarsPoster from "../../public/img01.jpg"
 
-export default function Card() {
-    return <div className="card" style={{
-        
-    }}>
-                <img src={StarWarsPoster} alt="Poster: Star Wars (1977)"/>
+export default function Card({poster, title}) {
+    return <div className="card">
+                <img src={poster} alt="Poster: Star Wars (1977)"/>
 
                 <div className="container-txt">
                     <div className="container">
-                        <h1>Poster: Star Wars (1977)</h1>
+                        <h1>{title}</h1>
                     </div>
                     <div className="container">
                         <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Esse clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
