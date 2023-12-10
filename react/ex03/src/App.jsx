57057787fba3fdf7d2ba16100bc5bed4511e17a1
Yago Machado <contato.yagomachado@outlook.com>
2023-12-10 11:10:30 -0300

    Feat ex03

--- react/ex03/src/App.jsx
+++ react/ex03/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import Profile from './components/Profile'
-import './App.css'
 import ProfileLogo from "../public/yago-img.jpg"
+ 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Profile avatar={ProfileLogo}></Profile>
+     <Profile 
+     avatar={ProfileLogo}
+     name="Yago Machado"
+     bio="Front-end Developer (Javascript, React, Sass & Node)"
+     phone="(21) 990598268"
+     email="contato.yagomachado@outlook.com"
+     githubUrl="https://github.com/developeryago"
+     linkedinUrl="https://www.linkedin.com/in/developeryago/"
+     instagramUrl="https://www.instagram.com/o.yagomachado"></Profile>
     </>
   )
 }
--- react/ex03/src/components/Profile/index.jsx
+++ react/ex03/src/components/Profile/index.jsx
@@ -1,24 +1,26 @@
-export default function Profile ({avatar, name, bio, email, phone, githubUrl, linkedinUrl, twitterUrl}) {
-    return <div className="containerProfile">
-            <div className="containerImg">
+import Style from "../../styles/css/style.module.css"
+
+export default function Profile ({avatar, name, bio, email, phone, linkedinUrl, twitterUrl, githubUrl, instagramUrl}) {
+    return <div className={Style.containerProfile}>
+            <div className={Style.containerImg}>
                 <img src={avatar} alt={name} />
             </div>
             <h3>{name}</h3>
 
-            <div className="containerDescription">
+            <div className={Style.containerDescription}>
                 <p>{bio}</p>
             </div>
-            <div className="containerDescription">
+            <div className={Style.containerDescription}>
                 <p>{email}</p>
             </div>
-            <div className="containerDescription">
+            <div className={Style.containerDescription}>
                 <p>{phone}</p>
             </div>
 
-            <div className="links">
-                <button>{githubUrl}</button>
-                <button>{linkedinUrl}</button>
-                <button>{twitterUrl}</button>
+            <div className={Style.links}>
+                <a href={githubUrl}>GitHub</a>
+                <a href={linkedinUrl}>Linkedin</a>
+                <a href={instagramUrl}>Instagram</a>
             </div>
         </div>
 }
