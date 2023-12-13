b09461bf2024a2281ffa18f71081f3a9bde5075f
Yago Machado <contato.yagomachado@outlook.com>
2023-12-13 13:20:53 -0300

    Aula sobre o uso de States

--- react/ex03/src/components/Profile/index.jsx
+++ react/ex03/src/components/Profile/index.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react"
 import Style from "../../styles/css/style.module.css"
 
-export default function Profile ({avatar, name, bio, email, phone, linkedinUrl, twitterUrl, githubUrl, instagramUrl}) {
+
+
+export default function Profile ({avatar, name, bio, email, phone, linkedinUrl, githubUrl, instagramUrl}) {
+    // (valor, funçãoModificadora)
+    const [followButtonTxt, setFollowButtonTxt] = useState("Follow")
+
+    function handleClick(ev) {
+        alert("Você clicou em Follow")
+        setFollowButtonTxt("Following")
+    }
+
     return <div className={Style.containerProfile}>
             <div className={Style.containerImg}>
                 <img src={avatar} alt={name} />
             </div>
             <h3>{name}</h3>
+            <button
+            onClick={handleClick}
+            className={Style.followButton}
+            >
+            {followButtonTxt}
+            </button>
 
             <div className={Style.containerDescription}>
                 <p>{bio}</p>
