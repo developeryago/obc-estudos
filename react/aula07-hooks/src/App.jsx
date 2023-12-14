f91988f852ffcf3184e98d542b83d76150c5bc16
Yago Machado <contato.yagomachado@outlook.com>
2023-12-14 17:27:26 -0300

    Aula sobre uso de useState com funções anônimas

--- react/aula07-hooks/src/App.jsx
+++ react/aula07-hooks/src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import useCounter from './hooks/useCounter'
 import './App.css'
-import { useState } from 'react'
 
 function App() {
   const count = useCounter()
--- react/aula07-hooks/src/hooks/useCounter.jsx
+++ react/aula07-hooks/src/hooks/useCounter.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 
+function getIntervalValue() {
+    console.log("Obtendo valor incial")
+    return 1 +1
+}
+
 export default function useCounter() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(getIntervalValue)
 
     const increment = () => {
-        setCount(count +1)
+        setCount((currentState) => currentState + 2)
     }
     return {count, increment}
 }
