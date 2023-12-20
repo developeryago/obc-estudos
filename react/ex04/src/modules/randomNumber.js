9a1caa7a70149bc909a369911b1f5b34b58d0352
Yago Machado <contato.yagomachado@outlook.com>
2023-12-20 08:06:12 -0300

    Update randomNumber.js

--- react/ex04/src/modules/randomNumber.js
+++ react/ex04/src/modules/randomNumber.js
@@ -2,7 +2,7 @@ import { Random } from "random-js"
 
 export default function randomNumber() {
     const random = new Random()
-    const aleatoryNumber = random.string(5-8, "A-Z")
+    const aleatoryNumber = random.string(8, "123456789")
     return console.log(aleatoryNumber)
 }
 
