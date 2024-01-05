57482b1d0948e4d13ce39e774beb38cd22eea12c
Yago Machado <contato.yagomachado@outlook.com>
2024-01-05 16:05:06 -0300

    Feat debug
    debug func onChange( )

--- react/ex04/src/App.jsx
+++ react/ex04/src/App.jsx
@@ -7,87 +7,25 @@ import Footer from "./components/Footer"
 import Copywriting from "./components/Copywriting"
 import { useState } from "react"
 
-const onChange = (ev) => {
-  const { value, type, checked} = ev.target
-  let res = type === "checkbox" ? checked : value
-  console.log(res)
-  return res
-}
+
 
 function App() {
   const [password, setPassword] = useState("")
-  let newPassword = ""
-  const num = document.getElementById("num")
-  const str = document.getElementById("str")
-  const alphaNum = document.getElementById("alphaNum")
 
-  function randomNumber() {
-    const random = new Random()
-    newPassword = random.string(8, "123456789")
-    console.log(newPassword)
-  }
   
-  function randomString() {
-    const random = new Random()
-    newPassword= random.string(8, 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstwxyz")
-    setPassword(newPassword)
-  }
 
-  function randomAlphanumeric() {
-    const random = new Random()
-    newPassword= random.string(8, 
-        "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstwxyz")
-    setPassword(newPassword)
-  }
-
-
-  function createPassword () {
-      if (res === "num") {
-        console.log(res)
-      } else if (res === "str") {
-        randomString()
-      } else if (res === "alphaNum") {
-        randomAlphanumeric()
-      }
-    }
 
   return (
     <>
     <div className="body">
       <div className="container">
 
-      <div className={"containerTitle"}>
-          Gerador de Senhas
-        </div>
+      <Title></Title>
 
-      <div className={"description"}>
-        <h3>Selecione uma opção abaixo:</h3>
-      </div>
+      <Description></Description>
 
-      <div className={"radioButtons"}>
-
-            <div className={"containerRadios"}>
-            <label htmlFor="num">Senha numérica</label>
-            <input onChange={onChange} type="radio" name="radio" id="num" value="num"/>
-            </div>
-
-            <div className={"containerRadios"}>
-            <label htmlFor="str">Senha com letras</label>
-            <input onChange={onChange} type="radio" name="radio" id="str" value="str"/>
-            </div>
-
-            <div className={"containerRadios"}>
-            <label htmlFor="alphaNum">Senha alfanumérica</label>
-            <input onChange={onChange} type="radio" name="radio" id="alphaNum" value="alphaNum"/>
-            </div>
-            
-        </div>
-
-        <div className={"buttons"}>
-          <button onClick={createPassword}>Gerar</button>
-        </div>
-    
+      <RadioButtons></RadioButtons>
+        
 
       <div class={"value"}>
           <input type="text" id={"result"} 
--- react/ex04/src/components/RadioButtons/index.jsx
+++ react/ex04/src/components/RadioButtons/index.jsx
@@ -1,21 +1,42 @@
 import style from "./style.module.css"
+import { useState } from "react"
+import randomNumber from "../../modules/randomNumber"
+import randomString from "../../modules/randomString"
+import randomAlphanumeric from "../../modules/randomAlphanumeric"
 
-export default function RadioButtons() {
+export default function RadioButtons(pros) {
+
+    
+
+ 
+  const onChange = (ev) => {
+    const { value, type, checked} = ev.target
+    let res = type === "checkbox" ? checked : value
+    if (res === "num") {
+      props.setPassword(randomNumber())
+    } else if (res === "str") {
+      setPassword(randomString())
+    } else if (res === "alphaNum") {
+      setPassword(randomAlphanumeric())
+    }
+    return res
+  }
+
     return <div className={style.radioButtons}>
 
             <div className={style.containerRadios}>
             <label htmlFor="num">Senha numérica</label>
-            <input type="radio" name="radio" id="num" />
+            <input onChange={onChange} type="radio" name="radio" id="num" />
             </div>
 
             <div className={style.containerRadios}>
             <label htmlFor="str">Senha com letras</label>
-            <input type="radio" name="radio" id="str" />
+            <input onChange={onChange} type="radio" name="radio" id="str" />
             </div>
 
             <div className={style.containerRadios}>
             <label htmlFor="alphaNum">Senha alfanumérica</label>
-            <input type="radio" name="radio" id="alphaNum" />
+            <input onChange={onChange} type="radio" name="radio" id="alphaNum" />
             </div>
             
         </div>
